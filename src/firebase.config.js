93b26303e75f1b598be6e8c2ab6cc7b4fb05c20d
Pratik Dash <141898165+pratikvv2@users.noreply.github.com>
2025-03-30 21:00:28 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -10,13 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // eslint-disable-next-line no-undef
 
 let   firebaseConfig = {
-        apiKey: "AIzaSyAPHEDwxqbx10wtmtSUUIy--F6F4dyjCvk",
-        authDomain: "getmobileiv.firebaseapp.com",
-        projectId: "getmobileiv",
-        storageBucket: "getmobileiv.appspot.com",
-        messagingSenderId: "743395832799",
-        appId: "1:743395832799:web:8cacb44e650ecaa8eb4a6a",
-        measurementId: "G-8S4YGJT51T"
+       apiKey: "AIzaSyCLwv3qliyb_sWF7HAhc6mfu0d4zRcEki8",
+        authDomain: "ivhydration-test.firebaseapp.com",
+        projectId: "ivhydration-test",
+        storageBucket: "ivhydration-test.appspot.com",
+        messagingSenderId: "277137861012",
+        appId: "1:277137861012:web:41a5b572c1c8b27c734781"
     };
 
 
